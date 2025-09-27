3798814ec262ac26cc1600ec4c5c1ba3612ee913
iamthewalrusz <165580771+iamthewalrusz@users.noreply.github.com>
2025-09-27 20:19:07 -0300

    Update views.py

--- project/mainApp/views.py
+++ project/mainApp/views.py
@@ -95,6 +95,60 @@ class PhysicalBooksViewSet(viewsets.ModelViewSet):
     queryset = PhysicalBooks.objects.all()
     serializer_class = PhysicalBooksSerializer
 
+    def list(self, request, *args, **kwargs):
+        # Regras de negócio
+
+        # Fim de regras de negócio
+
+        users = PhysicalBooks.objects.all()
+        serializer = PhysicalBooksSerializer(users, many=True)
+        return Response({ 'result': serializer.data }, status=HTTP_200_OK)
+
+
+    def create(self, request, *args, **kwargs):
+        print(request.data)
+        meta_book = request.data.get('meta_book')
+        owner = request.data.get('owner')
+        created_at = request.data.get('created_at')
+
+        if meta_book and owner and created_at:
+            physical_book = PhysicalBooks.objects.create_physical_book(meta_book=meta_book, owner=owner, created_at=created_at)
+            serializer = PhysicalBooksSerializer(physical_book)
+            return Response({ 'result': serializer.data }, status=HTTP_200_OK)
+        else:
+            return Response({ 'result': 'Dados inválidos'}, status=HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk, *args, **kwargs):
+        # Regras de negócio
+
+        # Fim de regras de negócio
+
+        instance = get_object_or_404(PhysicalBooks, pk=pk)
+        serializer = self.get_serializer(instance)
+        return Response({ 'result': serializer.data }, status=HTTP_200_OK)
+
+    def partial_update(self, request, pk, *args, **kwargs):
+        # Regras de negócio
+
+        # Fim de regras de negócio
+
+        instance = get_object_or_404(PhysicalBooks, pk=pk)
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response({ 'result': serializer.data }, status=HTTP_200_OK)
+    
+    def destroy(self, request, pk, *args, **kwargs):
+        # Regras de negócio
+
+        # Fim de regras de negócio
+
+        instance = get_object_or_404(PhysicalBooks, pk=pk)
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        self.destroy(instance)
+        return Response(serializer.data)
+
 
 class TransactionsViewSet(viewsets.ModelViewSet):
     queryset = Transactions.objects.all()
