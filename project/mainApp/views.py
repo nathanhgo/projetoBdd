b2a54e37950f07103573138f20ba947cbac45ec4
iamthewalrusz <165580771+iamthewalrusz@users.noreply.github.com>
2025-09-28 17:46:28 -0300

    transaction_physicalbook viewset atualizado

--- project/mainApp/views.py
+++ project/mainApp/views.py
@@ -333,7 +333,7 @@ class TransactionsViewSet(viewsets.ModelViewSet):
 class Transaction_PhysicalBookViewSet(viewsets.ModelViewSet):
     queryset = Transaction_PhysicalBook.objects.all()
     serializer_class = Transaction_PhysicalBookSerializer
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticated]
 
     def list(self, request, *args, **kwargs):
         # Regras de negócio
@@ -367,24 +367,11 @@ class Transaction_PhysicalBookViewSet(viewsets.ModelViewSet):
         serializer = self.get_serializer(instance)
         return Response({ 'result': serializer.data }, status=HTTP_200_OK)
 
-    def partial_update(self, request, pk, *args, **kwargs):
-        # Regras de negócio
+    def update(self, request, *args, **kwargs):
+        return Response({'result': 'Atualização não permitida'}, status=HTTP_400_BAD_REQUEST)
 
-        # Fim de regras de negócio
+    def partial_update(self, request, *args, **kwargs):
+        return Response({'result': 'Atualização não permitida'}, status=HTTP_400_BAD_REQUEST)
 
-        instance = get_object_or_404(Transaction_PhysicalBook, pk=pk)
-        serializer = self.get_serializer(instance, data=request.data, partial=True)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
-        return Response({ 'result': serializer.data }, status=HTTP_200_OK)
-    
-    def destroy(self, request, pk, *args, **kwargs):
-        # Regras de negócio
-
-        # Fim de regras de negócio
-
-        instance = get_object_or_404(Transaction_PhysicalBook, pk=pk)
-        serializer = self.get_serializer(instance, data=request.data, partial=True)
-        serializer.is_valid(raise_exception=True)
-        self.destroy(instance)
-        return Response({'result': serializer.data}, status=HTTP_200_OK)
+    def destroy(self, request, *args, **kwargs):
+        return Response({'result': 'Exclusão não permitida'}, status=HTTP_400_BAD_REQUEST)
