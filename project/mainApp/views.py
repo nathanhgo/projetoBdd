bca811601c8d7538ecf51dcd942f06d268aa37b3
comeraperuibe944 <63610807+comeraperuibe944@users.noreply.github.com>
2025-09-28 15:41:21 -0300

    add transaction view set

--- project/mainApp/views.py
+++ project/mainApp/views.py
@@ -230,7 +230,46 @@ class PhysicalBooksViewSet(viewsets.ModelViewSet):
 class TransactionsViewSet(viewsets.ModelViewSet):
     queryset = Transactions.objects.all()
     serializer_class = TransactionsSerializer
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticated]
+
+    def create(self, request, *args, **kwargs):
+        user = request.user
+        old_owner_id = request.data.get('old_owner')
+        new_owner_id = request.data.get('new_owner')
+
+        if str(user.id) not in [str(old_owner_id), str(new_owner_id)]:
+            return Response({'result': 'Você deve ser um dos usuários da transação'}, status=HTTP_400_BAD_REQUEST)
+
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response({'result': serializer.data}, status=HTTP_201_CREATED)
+
+    def retrieve(self, request, pk=None, *args, **kwargs):
+        instance = get_object_or_404(Transactions, pk=pk)
+        user = request.user
+
+        if user != instance.old_owner and user != instance.new_owner:
+            return Response({'result': 'Acesso negado'}, status=HTTP_400_BAD_REQUEST)
+
+        serializer = self.get_serializer(instance)
+        return Response({'result': serializer.data}, status=HTTP_200_OK)
+
+    def list(self, request, *args, **kwargs):
+        user = request.user
+        transactions = Transactions.objects.filter(old_owner=user) | Transactions.objects.filter(new_owner=user)
+        serializer = self.get_serializer(transactions, many=True)
+        return Response({'result': serializer.data}, status=HTTP_200_OK)
+
+    def update(self, request, *args, **kwargs):
+        return Response({'result': 'Atualização não permitida'}, status=HTTP_400_BAD_REQUEST)
+
+    def partial_update(self, request, *args, **kwargs):
+        return Response({'result': 'Atualização não permitida'}, status=HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, *args, **kwargs):
+        return Response({'result': 'Exclusão não permitida'}, status=HTTP_400_BAD_REQUEST)
+
 
 
 class Transaction_PhysicalBookViewSet(viewsets.ModelViewSet):
